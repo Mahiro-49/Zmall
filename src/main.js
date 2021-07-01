e8b9fc1160f8528692948d45ca4956518963e81b
KOTIN\Administrator <1019268193@qq.com>
2021-07-01 17:34:49 +0800

    '我的部分完成'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyLoaf from 'vue-lazyload'
 
 import toast from 'components/common/toast'
 
@@ -12,6 +13,11 @@ Vue.prototype.$bus = new Vue()
 
 // 安装toast插件
 Vue.use(toast)
+
+// 使用懒加载的插件
+Vue.use(VueLazyLoaf, {
+    loading: require('./assets/img/common/load.gif')
+})
 
 new Vue({
     render: h => h(App),
---
+++ src/views/profile/childComps/UserInfo.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="user">
+    <a href="#" class="clear-fix">
+      <slot name="user-icon">
+        <svg class="Image-svg left">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#avatar-default"></use>
+        </svg>
+      </slot>
+      <div class="login-info left">
+        <slot name="user-nickname">
+          <div>登陆/注册</div>
+        </slot>
+        <div class="phone">
+          <span>
+            <svg data-v-735ff1be="" fill="#fff" class="icon-mobile"><use data-v-735ff1be="" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#mobile"></use></svg>
+          </span>
+          <div class="uphone">
+            <slot name="user-phone">暂无绑定手机号</slot>
+          </div>
+        </div>
+      </div>
+      <svg data-v-735ff1be="" fill="#fff" class="arrow-svg right"><use data-v-735ff1be="" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use></svg>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo'
+}
+</script>
+
+<style scoped>
+.user {
+  background-color: var(--color-tint);
+  padding: 15px;
+  margin-top: -5px;
+}
+
+.Image-svg{
+  width: 60px;
+  height: 60px;
+  background-color: #fff;
+  border-radius: 30xp;
+}
+
+.left {
+  float: left;
+}
+
+.arrow-svg {
+  width: 11px;
+  height: 22px;
+  margin-top: 18px;
+}
+
+.login-info {
+  color: #fff;
+  margin: 10px 0 0 10px;
+  font-size: 13px;
+}
+
+.phone {
+  position: relative;
+  font-size: 13px;
+  margin-top: 5px;
+  margin-left: 15px;
+  font-weight: 300;
+}
+
+.uphone {
+  vertical-align: inherit;
+  font-size: 13px;
+  font-weight: 300;
+}
+
+.icon-mobile {
+  position: absolute;
+  width: 12px;
+  height: 18px;
+  left: -15px;
+  top: 0;
+}
+</style>
